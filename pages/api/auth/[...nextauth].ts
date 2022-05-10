302cb839acd731702a9ce84c5762935c70d1e14f
Neil Gebhard <neilgebhard@gmail.com>
2022-05-10 16:22:54 +1000

    :wrench: Fix typing error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -32,7 +32,7 @@ const options = {
   events: { createUser: sendWelcomeEmail },
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    async session({ session, token, user }) {
+    async session({ session, user }: { session: any; user: any }) {
       session.user.id = user.id
       return session
     },
