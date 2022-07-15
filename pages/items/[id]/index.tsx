4105641e9e3e10fbba847329ec6f76778c0f3c3d
Neil Gebhard <neilgebhard@gmail.com>
2022-07-15 17:56:50 +1000

    :sparkles: Add some buy logic

--- pages/items/[id]/index.tsx
+++ pages/items/[id]/index.tsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import AuthModal from '../../../components/AuthModal'
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const ids = await prisma.item.findMany({
@@ -50,9 +51,14 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
 const ListedItem = (item: Item) => {
   const { data: session } = useSession()
+  const user = session?.user
   const [isOwner, setIsOwner] = useState(false)
   const [deleting, setDeleting] = useState(false)
+  const [showModal, setShowModal] = useState(false)
   const router = useRouter()
+
+  const openModal = () => setShowModal(true)
+  const closeModal = () => setShowModal(false)
 
   useEffect(() => {
     const checkIfOwner = async () => {
@@ -80,55 +86,62 @@ const ListedItem = (item: Item) => {
     }
   }
 
+  const buyItem = () => {
+    session?.user ? alert('Item has been bought!') : openModal()
+  }
+
   return (
     <Layout>
-      <div className='max-w-screen-lg mx-auto'>
-        <div className='flex flex-col lg:flex-row gap-6'>
-          <div className='relative aspect-1 bg-gray-200 rounded-lg shadow-md overflow-hidden max-w-sm flex-1'>
-            {item?.image && (
-              <Image
-                src={item.image}
-                alt={item.name}
-                layout='fill'
-                objectFit='cover'
-                priority
-              />
-            )}
-          </div>
-          <div className='flex-1 text-lg'>
-            <h1 className='text-2xl font-bold'>{item?.name}</h1>
-            <p className='text-2xl'>
-              {new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
-              }).format(item?.price)}
-            </p>
-            <p className='mt-6'>{item?.description}</p>
-            {isOwner && (
-              <div className='flex items-center gap-x-2 mt-6'>
-                <Link href={`/items/${item.id}/edit`}>
-                  <a className='px-4 py-1 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition rounded-md disabled:text-gray-800 disabled:bg-transparent disabled:opacity-50 disabled:cursor-not-allowed'>
-                    Edit
-                  </a>
-                </Link>
-                <button
-                  type='button'
-                  disabled={deleting}
-                  onClick={() => deleteItem()}
-                  className='rounded-md border border-rose-500 text-rose-500 hover:bg-rose-500 hover:text-white focus:outline-none transition disabled:bg-rose-500 disabled:text-white disabled:opacity-50 disabled:cursor-not-allowed px-4 py-1'
-                >
-                  {deleting ? 'Deleting...' : 'Delete'}
-                </button>
-              </div>
-            )}
+      <div className='flex flex-col lg:flex-row gap-6'>
+        <div className='relative aspect-1 bg-gray-200 rounded-lg shadow-md overflow-hidden max-w-sm flex-1'>
+          {item?.image && (
+            <Image
+              src={item.image}
+              alt={item.name}
+              layout='fill'
+              objectFit='cover'
+              priority
+            />
+          )}
+        </div>
+        <div className='flex-1 text-lg'>
+          <h1 className='text-2xl font-bold'>{item?.name}</h1>
+          <p className='text-2xl'>
+            {new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            }).format(item?.price)}
+          </p>
+          <p className='mt-6'>{item?.description}</p>
+          {isOwner ? (
+            <div className='flex items-center gap-x-2 mt-6'>
+              <Link href={`/items/${item.id}/edit`}>
+                <a className='px-4 py-1 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition rounded-md disabled:text-gray-800 disabled:bg-transparent disabled:opacity-50 disabled:cursor-not-allowed'>
+                  Edit
+                </a>
+              </Link>
+              <button
+                type='button'
+                disabled={deleting}
+                onClick={() => deleteItem()}
+                className='rounded-md border border-rose-500 text-rose-500 hover:bg-rose-500 hover:text-white focus:outline-none transition disabled:bg-rose-500 disabled:text-white disabled:opacity-50 disabled:cursor-not-allowed px-4 py-1'
+              >
+                {deleting ? 'Deleting...' : 'Delete'}
+              </button>
+            </div>
+          ) : (
             <div className='mt-6'>
-              <button className='px-4 py-1 rounded-md bg-amber-600 hover:bg-amber-500 focus:outline-none focus:ring-4 focus:ring-amber-500 focus:ring-opacity-50 text-white transition'>
+              <button
+                onClick={buyItem}
+                className='px-4 py-1 rounded-md bg-amber-600 hover:bg-amber-500 focus:outline-none focus:ring-4 focus:ring-amber-500 focus:ring-opacity-50 text-white transition'
+              >
                 Buy
               </button>
             </div>
-          </div>
+          )}
         </div>
       </div>
+      <AuthModal show={showModal} onClose={closeModal} />
     </Layout>
   )
 }
